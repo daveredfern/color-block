1d8071a75398357ae36a76b78e66250bd1e90e2b
Dave Redfern <dave@daveredfern.com>
2016-07-09 14:50:00 +0100

    nav styling

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 	<div class="u-container">
 		<h2>Contact me</h2>
-		<?php wp_nav_menu( array( 'theme_location' => 'footer', 'container' => '', 'menu_id' => '' ) ); ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'u-list-unstyled', 'container' => '', 'menu_id' => '' ) ); ?>
 	</div>
-
-	<?php wp_footer(); ?>
 
 </body>
 </html>
