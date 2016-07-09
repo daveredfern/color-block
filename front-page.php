cbefb7ecfb742a656a66cf462a1cc5f3a2971e3c
Dave Redfern <dave@daveredfern.com>
2016-07-09 13:57:52 +0100

    nav styling

--- front-page.php
+++ front-page.php
@@ -27,40 +27,16 @@
 		<p>Sorry, but you are looking for something that isn't here.</p>
 	</div>
 <?php endif; ?>
-<div class="u-container u-container--lg u-section">
-	<hr />
-</div>
-<div class="g-two-up g-limit">
-	<div>
-		<h2>Recent articles</h2>
-		<ul class="u-list-space">
-	    <?php
-            $args = array(
-				'post_type' => 'post',
-                'posts_per_page' => 5
-            );
-	        $latestarticles = new WP_Query( $args );
 
-	        while( $latestarticles->have_posts() ) {
-	            $latestarticles->the_post();
-	        ?>
-	        	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-	        <?php
-	        }
-	        wp_reset_query();
-	    ?>
-		</ul>
-	</div>
-	<div>
-		<h2>Contact me</h2>
-		<p>For a quick response I recommend tweeting me. For a more indepth conversation feel free to send an email.</p>
-		<ul>
-			<li><a href="https://www.twitter.com/daveredfern">@daveredfern</a></li>
-			<li><a href="mailto:dave@daveredfern.com">dave@daveredfern.com</a></li>
-			<li><a href="https://github.com/daveredfern">Github</a></li>
-			<li><a href="https://uk.linkedin.com/in/daveredfern">LinkedIn</a></li>
-		</ul>
-	</div>
+<div class="u-container">
+	<h2>Contact me</h2>
+	<p>For a quick response I recommend tweeting me. For a more indepth conversation feel free to send an email.</p>
+	<ul>
+		<li><a href="https://www.twitter.com/daveredfern">@daveredfern</a></li>
+		<li><a href="mailto:dave@daveredfern.com">dave@daveredfern.com</a></li>
+		<li><a href="https://github.com/daveredfern">Github</a></li>
+		<li><a href="https://uk.linkedin.com/in/daveredfern">LinkedIn</a></li>
+	</ul>
 </div>
 
 <?php get_footer(); ?>
