32ee38b4d4d60609b1ebffcc102c252d096be43b
Dave Redfern <dave@daveredfern.com>
2016-07-12 08:50:34 +0100

    Adding footer about

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
 	<div class="u-container">
-		<h2>Contact me</h2>
-		<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'u-list-unstyled', 'container' => '', 'menu_id' => '' ) ); ?>
+		<div class="g-two-up">
+			<div>
+				<h2>About me</h2>
+				<p>I work for British Gymnastics, the governing body for gymnastics in the UK. As part of the Marketing and Communications team I help promote events and recreational gymnastics to all ages.</p>
+			</div>
+			<div>
+				<h2>Contact me</h2>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'u-list-unstyled', 'container' => '', 'menu_id' => '' ) ); ?>
+			</div>
+		</div>
 	</div>
 
 </body>
