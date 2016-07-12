0303020a48c4f9eb12e7becffdde51475ccad055
Dave Redfern <dave@daveredfern.com>
2016-07-12 08:53:06 +0100

    Widening gird on footer

--- footer.php
+++ footer.php
@@ -1,13 +1,11 @@
-	<div class="u-container">
-		<div class="g-two-up">
-			<div>
-				<h2>About me</h2>
-				<p>I work for British Gymnastics, the governing body for gymnastics in the UK. As part of the Marketing and Communications team I help promote events and recreational gymnastics to all ages.</p>
-			</div>
-			<div>
-				<h2>Contact me</h2>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'u-list-unstyled', 'container' => '', 'menu_id' => '' ) ); ?>
-			</div>
+	<div class="g-two-up">
+		<div>
+			<h2>About me</h2>
+			<p>I work for British Gymnastics, the governing body for gymnastics in the UK. As part of the Marketing and Communications team I help promote events and recreational gymnastics to all ages.</p>
+		</div>
+		<div>
+			<h2>Contact me</h2>
+			<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_class' => 'u-list-unstyled', 'container' => '', 'menu_id' => '' ) ); ?>
 		</div>
 	</div>
 
