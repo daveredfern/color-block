a87df1d188c93d7e1ef3332ee3528c0178fbf98a
Dave Redfern <dave@daveredfern.com>
2016-07-26 16:32:13 +0100

    Adding hover to projects

--- single.php
+++ single.php
@@ -16,7 +16,7 @@
 		<?php elseif( get_row_layout() == 'portfolio_block_image' ): ?>
 			<div class="u-container u-section" style="background-color: <?php the_sub_field('portfolio_background_color'); ?>">
 				<?php
-					$image = get_sub_field('featured_foreground_image');
+					$image = get_sub_field('portfolio_image');
 					print_r($image);
 				?>
 				<img src="<?php echo $image['url']; ?>" />
