d9d2c6ecd59b3443b7194be8bc0212c47454f510
Dave Redfern <dave@daveredfern.com>
2016-07-26 13:21:53 +0100

    Adding hover to projects

--- footer.php
+++ footer.php
@@ -1,18 +1,16 @@
-	<div class="g-two-up">
-		<div>
-			<h2>About me</h2>
-			<p>I work for British Gymnastics, the governing body for gymnastics in the UK. As part of the Marketing and Communications team I help promote events and recreational gymnastics to all ages.</p>
-		</div>
-		<div>
-			<h2>Contact me</h2>
-			<ul class="u-list-unstyled">
-				<li>Email <a href="mailto:karenuppal05@gmail.com">karenuppal05@gmail.com</a></li>
-				<li>Tweet <a href="http://twitter.com/karenuppal05">@karenuppal05</a></li>
-				<li><a href="https://uk.linkedin.com/in/karen-uppal-14999466">LinkedIn</a></li>
-				<li><a href="https://www.instagram.com/karen.uppal/">Instagram</a></li>
-			</ul>
-		</div>
+
+	<div class="u-container">
+		<p class="lead">I work for British Gymnastics, the governing body for gymnastics
+in the UK. As part of the Marketing and Communications team I help 
+promote events and recreational gymnastics to all ages.</p>
 	</div>
+
+	<ul class="footer-menu">
+		<li><a href="mailto:karenuppal05@gmail.com">Email me</a></li>
+		<li><a href="http://twitter.com/karenuppal05">Tweet me</a></li>
+		<li><a href="https://uk.linkedin.com/in/karen-uppal-14999466">LinkedIn</a></li>
+		<li><a href="https://www.instagram.com/karen.uppal/">Instagram</a></li>
+	</ul>
 
 </body>
 </html>
