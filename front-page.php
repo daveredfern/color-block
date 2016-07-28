934ec3c872164c1bf678c0f642c6f021db146781
Dave Redfern <dave@daveredfern.com>
2016-07-28 20:41:03 +0100

    Mods

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="u-section">
+	<div class="u-section u-container-wide">
 		<div class="welcome">
 			<div class="welcome__header">
 				<div class="logo">
