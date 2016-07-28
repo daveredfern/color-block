b6895f1f22f76552d970f5bd9297256f1283edfa
Dave Redfern <dave@daveredfern.com>
2016-07-28 21:07:37 +0100

    Changes

--- page.php
+++ page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="u-container  u-section-end u-center">
+	<div class="u-container u-section-end u-center">
 		<div class="u-section">
 			<h1 class="main-title"><?php the_title(); ?></h1>
 		</div>
 		<?php the_content(); ?>
 	</div>
 <?php endwhile; else : ?>
-	<div class="u-container">
+	<div class="u-container u-center">
 		<h1>Not Found</h1>
 		<p>Sorry, but you are looking for something that isn't here.</p>
 	</div>
