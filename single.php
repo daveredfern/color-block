41812792b90738c1e7ff9040d8b5cba1b07bc8dd
Dave Redfern <dave@daveredfern.com>
2016-08-14 18:13:34 +0100

    adding link to next

--- single.php
+++ single.php
@@ -52,7 +52,7 @@
 					?>
 						<div class="next-post">
 							<div class="u-container-wide u-section">
-								<p>Next</p>
+								<p><a href="<?php the_permalink(); ?>">Next</a></p>
 								<h2 class="main-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 							</div>
 						</div>
