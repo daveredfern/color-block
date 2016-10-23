c212f477bca9717c48958cc5c5ca1c3d2a1c6bfa
Dave Redfern <dave@daveredfern.com>
2016-10-23 14:35:12 +0100

    Removing scrolling

--- build/js/script.js
+++ build/js/script.js
@@ -1,20 +1,20 @@
-function runScroll() {
-  scrollTo(document.body, 0, 600);
-}
+// function runScroll() {
+//   scrollTo(document.body, 0, 600);
+// }
 
 
-var scrollme;
-scrollme = document.querySelector("#scrollme");
-scrollme.addEventListener("click",runScroll,false);
+// var scrollme;
+// scrollme = document.querySelector("#scrollme");
+// scrollme.addEventListener("click",runScroll,false);
 
-function scrollTo(element, to, duration) {
-  if (duration <= 0) return;
-  var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
+// function scrollTo(element, to, duration) {
+//   if (duration <= 0) return;
+//   var difference = to - element.scrollTop;
+//   var perTick = difference / duration * 10;
 
-  setTimeout(function() {
-    element.scrollTop = element.scrollTop + perTick;
-    if (element.scrollTop == to) return;
-    scrollTo(element, to, duration - 10);
-  }, 10);
-}
+//   setTimeout(function() {
+//     element.scrollTop = element.scrollTop + perTick;
+//     if (element.scrollTop == to) return;
+//     scrollTo(element, to, duration - 10);
+//   }, 10);
+// }
